f98e9339afc990f9b8d782e949bdf24bc0666dde
Zachary Berkley <75088138+ZBerkley88@users.noreply.github.com>
2022-11-18 20:34:32 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+const Manager = require('./lib/Engineer.js');
+
+
+function createTeam(){
+// prompt the user if they want to create more team members
+// addEngineer()
+// addIntern()
+
+}
+
+function addEngineer() {
+
+
+}
+
+function addIntern() {
+
+}
---
+++ pseudocode.js
@@ -0,0 +1,19 @@
+// create a generic "worker/employee" constructor function
+// create functions that extend worker/employee: intern, manager, engineer
+// create HTML for website
+// create CSS for website
+// create tests
+
+// employees
+    // name
+    // id
+    // email
+
+// manager 
+    // office number
+
+// engineer
+    // github username with link
+
+// intern
+    // school
