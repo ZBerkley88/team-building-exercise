5bf9e972bd414cf11e2d592933359dde2f45dd47
Zack <zachary.berkley@gmail.com>
2023-01-15 13:50:35 -0500

    wr\ote tests for employee.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,16 +1,40 @@
 const Employee = require('../lib/Employee')
 
-test('Can I pass name, ID, email to this new class instance constructor', () => {
-    const emp = new Employee('a', 'b', 'c')
-    expect(emp.name).toBe('a')
-    expect(emp.id).toBe('b')
-    expect(emp.email).toBe('c')
+test(`Can I pass name, ID, email to this new class instance constructor?`, () => {
+    const emp = new Employee(`Edward Scissorhands`, `12`, `Eddie@scissors.com`)
+    expect(emp.name).toBe(`Edward Scissorhands`)
+    expect(emp.id).toBe(`12`)
+    expect(emp.email).toBe(`Eddie@scissors.com`)
 })
 
-test('Can getName return Name ', () => {
+
+test(`Can getName return an employee's name?`, () => {
   
-    const emp = new Employee('a', 'b', 'c')
-    
-    expect(emp.getName()).toBe('a')
-    
+    const emp = new Employee(`Edward Scissorhands`, `12`, `Eddie@scissors.com`)
+    expect(emp.getName()).toBe(`Edward Scissorhands`)
+
+})
+
+
+test(`Can getName return an employee's ID number?`, () => {
+  
+    const emp = new Employee(`Edward Scissorhands`, `12`, `Eddie@scissors.com`)
+    expect(emp.getId()).toBe(`12`)
+
+})
+
+
+test(`Can getName return an employee's email address?`, () => {
+  
+    const emp = new Employee(`Edward Scissorhands`, `12`, `Eddie@scissors.com`)
+    expect(emp.getEmail()).toBe(`Eddie@scissors.com`)
+
+})
+
+
+test(`Can getName return an employee's role?`, () => {
+  
+    const emp = new Employee(`Edward Scissorhands`, `12`, `Eddie@scissors.com`)
+    expect(emp.getRole()).toBe(`Employee`)
+
 })
